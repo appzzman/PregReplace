470bdcf0ed308973e8f5f61f2ba73e2ac5d8de1b
izotx <janusz@izotx.com>
2015-05-08 05:30:12 -0500

    Saving changes to a local copy

--- index.php
+++ index.php
@@ -11,11 +11,26 @@
             return;
         }
         $handle = fopen($filepath, "c+");
+        $newFile = "copy".rand(0,1000).$filepath;
+        $write_handle = fopen($newFile,"x");
+        
+        if (!$handle || !$write_handle)
+        {
+            print_r( sprintf ( '%o', fileperms ( $file ) ), PHP_EOL);
+            print_r( posix_getpwuid ( fileowner ( $file ) ), PHP_EOL); // Get Owner
+            print_r( posix_getpwuid ( posix_getuid () ), PHP_EOL); // Get User
+           
+            echo "Most likely permission error occured.";
+            return;
+        }
+        
+        
         $string = file_get_contents($filepath);
         $count = null;
         $content = preg_replace($regex, $replacement, $string, -1, $count);
-        fwrite($handle, $content);
+        fwrite($write_handle, $content);
         fclose($handle);
-        echo "Completed. ".$count." found.";
+        fclose($write_handle);
+        echo "Completed. ".$count." found. Changes written to: ".$newFile;
     }  
 ?>
